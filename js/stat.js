9fa8f0c8ecda0367a55b606c235d4e85bc46265f
AlexejKupin <alexejkupin@gmail.com>
2017-08-21 18:21:17 +0300

    ДЗ 2 очередные правки
    Сейчас все вроде как надо

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,9 @@
 var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+var ctx2 = canvas.getContext('2d');
 var max;
+var myNameIndex;
+
+
 function getMaxValue(times){
   max = times[0]; 
     for (var i = 0; i < times.length; i++) { 
@@ -13,43 +16,52 @@ function getMaxValue(times){
 window.renderStatistics = function(ctx,names,times) {
 
 getMaxValue(times);
-ctx.rect(50, 5, 210,135);
-ctx.fillStyle = 'white';
-ctx.shadowColor ="rgba(0, 0, 0, 0.7)";
-ctx.shadowBlur = 4;
-ctx.shadowOffsetX = 5;
-ctx.shadowOffsetY = 5;
-ctx.fill();
+ctx2.rect(50, 5, 210,135);
+ctx2.fillStyle = 'white';
+ctx2.shadowColor ="rgba(0, 0, 0, 0.7)";
+ctx2.shadowBlur = 4;
+ctx2.shadowOffsetX = 5;
+ctx2.shadowOffsetY = 5;
+ctx2.fill();
 
-ctx.shadowBlur = 0;
-ctx.shadowOffsetX = 0;
-ctx.shadowOffsetY = 0;
-ctx.font = '16px "PT Mono"';
-ctx.shadowColor ="white";
-ctx.textBaseline = 'hanging';
-ctx.fillStyle = 'black';
-ctx.fillText('Вы победили!', 115, 10);
-ctx.fillText('Список результатов!', 90, 20);
+ctx2.shadowBlur = 0;
+ctx2.shadowOffsetX = 0;
+ctx2.shadowOffsetY = 0;
+ctx2.font = '16px "PT Mono"';
+ctx2.shadowColor ="white";
+ctx2.textBaseline = 'hanging';
+ctx2.fillStyle = 'black';
+ctx2.fillText('Вы победили!', 100, 10);
+ctx2.fillText('Список результатов!', 65, 20);
 
-ctx.font = '8px Arial';
-ctx.fillStyle = 'rgba(255,0,0,1)';
-ctx.fillRect(77, 120-75/max*times[0], 20, 75/max*times[0]);
-ctx.fillStyle = 'black';
-ctx.fillText(names[0], 77, 125);
+ctx2.font = '8px Arial';
 
-ctx.fillStyle = 'rgba(0,0,255,0.3)';
-ctx.fillRect(122, 120-75/max*times[1], 20, 75/max*times[1]);
-ctx.fillStyle = 'black';
-ctx.fillText(names[1], 122, 125);
+if (names[0] ==='Вы') {ctx2.fillStyle = 'rgba(255,0,0,1)';
+  } else {ctx2.fillStyle = 'rgba(0,0,255,'+ Math.random() +')';
+    };
+ctx2.fillRect(77, 120-75/max*times[0], 20, 75/max*times[0]);
+ctx2.fillStyle = 'black';
+ctx2.fillText(names[0], 77, 125);
 
-ctx.fillStyle = 'rgba(0,0,255,0.7)';
-ctx.fillRect(167, 120-75/max*times[2], 20,75/max*times[2]);
-ctx.fillStyle = 'black';
-ctx.fillText(names[2], 167, 125);
+if (names[1] ==='Вы') {ctx2.fillStyle = 'rgba(255,0,0,1)';
+  } else {ctx2.fillStyle = 'rgba(0,0,255,'+ Math.random() +')';
+    };
+ctx2.fillRect(122, 120-75/max*times[1], 20, 75/max*times[1]);
+ctx2.fillStyle = 'black';
+ctx2.fillText(names[1], 122, 125);
 
-ctx.fillStyle = 'rgba(0,0,255, 0.2)';
-ctx.fillRect(212, 120-75/max*times[3], 20,75/max*times[3]);
-ctx.fillStyle = 'black';
-ctx.fillText(names[3], 212, 125);
+if (names[2] ==='Вы') {ctx2.fillStyle = 'rgba(255,0,0,1)';
+  } else {ctx2.fillStyle = 'rgba(0,0,255,'+ Math.random() +')';
+    };
+ctx2.fillRect(167, 120-75/max*times[2], 20,75/max*times[2]);
+ctx2.fillStyle = 'black';
+ctx2.fillText(names[2], 167, 125);
+
+if (names[3] ==='Вы') {ctx2.fillStyle = 'rgba(255,0,0,1)';
+  } else {ctx2.fillStyle = 'rgba(0,0,255,'+ Math.random() +')';
+    };
+ctx2.fillRect(212, 120-75/max*times[3], 20,75/max*times[3]);
+ctx2.fillStyle = 'black';
+ctx2.fillText(names[3], 212, 125);
 };
 
