5b99f9cd1ec12cd785e06d972a9a595fffabb516
AlexejKupin <alexejkupin@gmail.com>
2017-08-22 18:19:43 +0300

    ДЗ 3
    добавил функцию по заполнению, все работает

--- js/setup.js
+++ js/setup.js
@@ -4,11 +4,22 @@ var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', '�
 var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)','rgb(215, 210, 55)','rgb(0, 0, 0)'];
 var WIZARD_EYES = ['black', 'red', 'blue', 'yellow','green',];
 
+function GamePlayer(n, c, e) {
+    this.name = n;
+    this.coatColor = c;
+    this.eyesColor = e;
+};
+
+var wizards = [];
+
+for (i = 0; i <= 4; i++) {
 var randName = WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)] +' '+ WIZARD_SURNAMES[Math.floor(Math.random() * WIZARD_SURNAMES.length)];
 var randCoat = WIZARD_COATS[Math.floor(Math.random() * WIZARD_COATS.length)];
 var randEyes = WIZARD_EYES[Math.floor(Math.random() * WIZARD_EYES.length)];
+  wizards[i] = new GamePlayer(randName.toString(),randCoat.toString(),randEyes.toString());
+};
 
-var wizards = [
+/*var wizards = [
   {
     name: WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)] +' '+ WIZARD_SURNAMES[Math.floor(Math.random() * WIZARD_SURNAMES.length)],
     coatColor: WIZARD_COATS[Math.floor(Math.random() * WIZARD_COATS.length)],
@@ -30,7 +41,7 @@ var wizards = [
     coatColor: WIZARD_COATS[Math.floor(Math.random() * WIZARD_COATS.length)],
     eyesColor : WIZARD_EYES[Math.floor(Math.random() * WIZARD_EYES.length)],
   },
-];
+];*/
 
 
 
