5e26c9896f8341818ab3194fe47d4c28f76ddeaa
AlexejKupin <alexejkupin@gmail.com>
2017-09-05 23:18:30 +0300

    Исправления канвы после консультации с Никитой

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,5 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+//var canvas = document.getElementById('canvas');
+//var ctx = canvas.getContext('2d');
 var max;
 var myNameIndex;
 
@@ -13,16 +13,28 @@ function getMaxValue(times){
 }
  
 
+var drawColum = function (ctx, number, height, revert) {
+ctx.fillStyle = 'rgba(255,0,0,1)';
+ctx.fillRect(140 + number * 90, revert, 40, height);
+
+}
+
 window.renderStatistics = function(ctx,names,times) {
 
+
+
 getMaxValue(times);
-ctx.rect(50, 5, 210,135);
+
 ctx.fillStyle = 'white';
 ctx.shadowColor ="rgba(0, 0, 0, 0.7)";
 ctx.shadowBlur = 4;
 ctx.shadowOffsetX = 5;
 ctx.shadowOffsetY = 5;
-ctx.fill();
+ctx.fillRect(100, 10, 420,270);
+
+for (var i = 0; i < 4; i++ ) {
+drawColum(ctx, i, 150/max*times[i], 240 - 150/max*times[i] ); 
+}; 
 
 ctx.shadowBlur = 0;
 ctx.shadowOffsetX = 0;
@@ -36,29 +48,30 @@ ctx.fillText('Список результатов!', 65, 20);
 
 ctx.font = '8px Arial';
 
-if (names[0] ==='Вы') {ctx2.fillStyle = 'rgba(255,0,0,1)';
-  } else {ctx2.fillStyle = 'rgba(0,0,255,'+ Math.random() +')';
+
+if (names[0] ==='Вы') {ctx.fillStyle = 'rgba(255,0,0,1)';
+  } else {ctx.fillStyle = 'rgba(0,0,255,'+ Math.random() +')';
     };
 ctx.fillRect(77, 120-75/max*times[0], 20, 75/max*times[0]);
 ctx.fillStyle = 'black';
 ctx.fillText(names[0], 77, 125);
 
-if (names[1] ==='Вы') {ctx2.fillStyle = 'rgba(255,0,0,1)';
-  } else {ctx2.fillStyle = 'rgba(0,0,255,'+ Math.random() +')';
+if (names[1] ==='Вы') {ctx.fillStyle = 'rgba(255,0,0,1)';
+  } else {ctx.fillStyle = 'rgba(0,0,255,'+ Math.random() +')';
     };
-ctx.fillRect(122, 120-75/max*times[1], 20, 75/max*times[1]);
+//ctx.fillRect(122, 120-75/max*times[1], 20, 75/max*times[1]);
 ctx.fillStyle = 'black';
 ctx.fillText(names[1], 122, 125);
 
-if (names[2] ==='Вы') {ctx2.fillStyle = 'rgba(255,0,0,1)';
-  } else {ctx2.fillStyle = 'rgba(0,0,255,'+ Math.random() +')';
+if (names[2] ==='Вы') {ctx.fillStyle = 'rgba(255,0,0,1)';
+  } else {ctx.fillStyle = 'rgba(0,0,255,'+ Math.random() +')';
     };
 ctx.fillRect(167, 120-75/max*times[2], 20,75/max*times[2]);
 ctx.fillStyle = 'black';
 ctx.fillText(names[2], 167, 125);
 
-if (names[3] ==='Вы') {ctx2.fillStyle = 'rgba(255,0,0,1)';
-  } else {ctx2.fillStyle = 'rgba(0,0,255,'+ Math.random() +')';
+if (names[3] ==='Вы') {ctx.fillStyle = 'rgba(255,0,0,1)';
+  } else {ctx.fillStyle = 'rgba(0,0,255,'+ Math.random() +')';
     };
 ctx.fillRect(212, 120-75/max*times[3], 20,75/max*times[3]);
 ctx.fillStyle = 'black';
